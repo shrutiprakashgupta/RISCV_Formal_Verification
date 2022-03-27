7572522fbee725af2fecc271ee7183b70dea71bb
shrutiprakashgupta <shrutiprakash999@gmail.com>
2022-03-27 23:23:25 +0530

    Multiplier Corrected

--- exe_stage/multiplier.sv
+++ exe_stage/multiplier.sv
@@ -15,12 +15,9 @@ input         id_b_signed_r,
 output [63:0] mul_res_w
 );
     
-wire mul_div_negative_w = (id_a_signed_r && id_ra_value_r[31]) ^ (id_b_signed_r && id_rb_value_r[31]);
 wire [31:0] mul_div_a_w = (id_a_signed_r && id_ra_value_r[31]) ? -id_ra_value_r : id_ra_value_r;
 wire [31:0] mul_div_b_w = (id_b_signed_r && id_rb_value_r[31]) ? -id_rb_value_r : id_rb_value_r;
     
-wire [63:0] mul_opa_a_w = { {32{id_a_signed_r & id_ra_value_r[31]}}, id_ra_value_r };
-wire [63:0] mul_opa_b_w = { {32{id_b_signed_r & id_rb_value_r[31]}}, id_rb_value_r };
-assign mul_res_w = mul_opa_a_w * mul_opa_b_w;
+assign mul_res_w = mul_div_a_w * mul_div_b_w;
 
 endmodule
